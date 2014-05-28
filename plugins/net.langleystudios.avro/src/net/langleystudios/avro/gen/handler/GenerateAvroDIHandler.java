0496e5132befff3df6fec3e9de584a733b42c5bc
J Langley <jlangley@cohesionforce.com>
2014-05-27 21:42:12 -0500

    Added compatibility layer to hook commands to right click menu on the package explorer. This closes #5.

---
+++ plugins/net.langleystudios.avro/src/net/langleystudios/avro/gen/handler/GenerateAvroDIHandler.java
@@ -0,0 +1,10 @@
+package net.langleystudios.avro.gen.handler;
+
+import org.eclipse.e4.tools.compat.parts.DIHandler;
+
+public class GenerateAvroDIHandler extends DIHandler<GenerateAvroHandler> {
+
+	public GenerateAvroDIHandler(Class<GenerateAvroHandler> clazz) {
+		super(clazz);
+	}
+}
---
+++ plugins/net.langleystudios.avro/src/net/langleystudios/avro/gen/handler/GenerateJavaDIHandler.java
@@ -0,0 +1,10 @@
+package net.langleystudios.avro.gen.handler;
+
+import org.eclipse.e4.tools.compat.parts.DIHandler;
+
+public class GenerateJavaDIHandler extends DIHandler<GenerateJavaHandler> {
+
+	public GenerateJavaDIHandler(Class<GenerateJavaHandler> clazz) {
+		super(clazz);
+	}
+}
