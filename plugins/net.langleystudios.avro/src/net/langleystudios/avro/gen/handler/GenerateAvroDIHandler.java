62522e205c66eaaa7c198bf7150c8eead97ae1cc
J Langley <jlangley@cohesionforce.com>
2014-05-31 11:48:33 -0500

    Updated the test to run, and fixed the issues with right clicking items and running commands.

--- plugins/net.langleystudios.avro/src/net/langleystudios/avro/gen/handler/GenerateAvroDIHandler.java
+++ plugins/net.langleystudios.avro/src/net/langleystudios/avro/gen/handler/GenerateAvroDIHandler.java
@@ -1,10 +1,42 @@
 package net.langleystudios.avro.gen.handler;
 
-import org.eclipse.e4.tools.compat.parts.DIHandler;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.CanExecute;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.ui.PlatformUI;
 
-public class GenerateAvroDIHandler extends DIHandler<GenerateAvroHandler> {
+public class GenerateAvroDIHandler extends AbstractHandler {
 
-	public GenerateAvroDIHandler(Class<GenerateAvroHandler> clazz) {
-		super(clazz);
+	GenerateAvroHandler handler;
+	public GenerateAvroDIHandler() {
+		 IEclipseContext context = getActiveContext();
+		 handler = ContextInjectionFactory.make(GenerateAvroHandler.class, context);
+	}
+	
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		 return ContextInjectionFactory.invoke(handler, Execute.class,
+				 getActiveContext());
+	}
+	
+	@Override
+	public boolean isHandled() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		 return (boolean) ContextInjectionFactory.invoke(handler, CanExecute.class,
+				 getActiveContext());
+	}
+
+	private static IEclipseContext getActiveContext() {
+		 IEclipseContext parentContext = (IEclipseContext) PlatformUI.getWorkbench().getService(
+		 IEclipseContext.class);
+		 return parentContext.getActiveLeaf();
 	}
 }
--- plugins/net.langleystudios.avro/src/net/langleystudios/avro/gen/handler/GenerateFromGenModelDIHandler.java
+++ plugins/net.langleystudios.avro/src/net/langleystudios/avro/gen/handler/GenerateFromGenModelDIHandler.java
@@ -1,18 +1,43 @@
 package net.langleystudios.avro.gen.handler;
 
+import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.e4.tools.compat.parts.DIHandler;
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.CanExecute;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.ui.PlatformUI;
 
-public class GenerateFromGenModelDIHandler extends DIHandler<GenerateFromGenModelHandler> {
+public class GenerateFromGenModelDIHandler extends AbstractHandler {
 
-	public GenerateFromGenModelDIHandler(Class<GenerateFromGenModelHandler> clazz) {
-		super(clazz);
+	GenerateFromGenModelHandler handler;
+	public GenerateFromGenModelDIHandler() {
+		 IEclipseContext context = getActiveContext();
+		 handler = ContextInjectionFactory.make(GenerateFromGenModelHandler.class, context);
 	}
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
-		return super.execute(event);
+		 return ContextInjectionFactory.invoke(handler, Execute.class,
+				 getActiveContext());
 	}
+	
+	@Override
+	public boolean isHandled() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		 return (boolean) ContextInjectionFactory.invoke(handler, CanExecute.class,
+				 getActiveContext());
+	}
+
+	private static IEclipseContext getActiveContext() {
+		 IEclipseContext parentContext = (IEclipseContext) PlatformUI.getWorkbench().getService(
+		 IEclipseContext.class);
+		 return parentContext.getActiveLeaf();
+	}
+	
 }
